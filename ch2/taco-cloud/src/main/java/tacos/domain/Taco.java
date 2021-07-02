5d343dc9771ed436f4e54991681a9e38b541b704
cusbert <mindychoi20@gmail.com>
2021-07-03 01:12:17 +0900

    feat: Add validation check

--- ch2/taco-cloud/src/main/java/tacos/domain/Taco.java
+++ ch2/taco-cloud/src/main/java/tacos/domain/Taco.java
@@ -2,12 +2,16 @@ package tacos.domain;
 
 import lombok.Data;
 
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
 public class Taco {
-    @NotBlank(message = "Name is required")
+    @NotNull
+    @Size(min=5, message = "Name must be at least 5 characters long")
     private String name;
+
+    @Size(min=1, message = "You must choose at least 1 ingredient")
     private List<String> ingredients;
 }
