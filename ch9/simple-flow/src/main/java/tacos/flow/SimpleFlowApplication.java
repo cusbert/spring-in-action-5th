1f0657053f38a0c4e210d0fbd7c5c4e8f1edddae
cusbert <mindychoi20@gmail.com>
2021-08-01 20:27:09 +0900

    first commit

---
+++ ch9/simple-flow/src/main/java/tacos/flow/SimpleFlowApplication.java
@@ -0,0 +1,13 @@
+package tacos.flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleFlowApplication.class, args);
+	}
+
+}
---
+++ ch9/simple-flow/src/test/java/tacos/flow/SimpleFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package tacos.flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
