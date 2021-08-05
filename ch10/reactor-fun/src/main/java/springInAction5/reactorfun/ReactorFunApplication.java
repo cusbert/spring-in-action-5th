71e50f8093f7e14faf232fb17da86b5d383ff8e0
cusbert <mindychoi20@gmail.com>
2021-08-05 23:53:53 +0900

    first commit

---
+++ ch10/reactor-fun/src/main/java/springInAction5/reactorfun/ReactorFunApplication.java
@@ -0,0 +1,13 @@
+package springInAction5.reactorfun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactorFunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactorFunApplication.class, args);
+	}
+
+}
---
+++ ch10/reactor-fun/src/test/java/springInAction5/reactorfun/ReactorFunApplicationTests.java
@@ -0,0 +1,13 @@
+package springInAction5.reactorfun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactorFunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
